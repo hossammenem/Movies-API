fd5cafa08a7c508e905fdc6d7084544b6ca698b2
hossammenem <88946630+hossammenem@users.noreply.github.com>
2023-01-20 15:46:10 +0200

    Update searchForMovie.ts

--- client/src/api/searchForMovie.ts
+++ client/src/api/searchForMovie.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_KEY = "23c2625d"
+import { API_KEY } from "./config"
 
 export default async function searchForMovie(title: String) {
     const response = await axios(`https://www.omdbapi.com/?s='${title.replace(' ', '+')}'&apikey=${API_KEY}`)
